b36a2a4fb4b085564b6036c293ca85065efaeab8
Dev Agent <dev@facturation.local>
2026-02-17 22:24:43 +0000

    chore: Bump version to 1.0.1

--- frontend/src/config/version.js
+++ frontend/src/config/version.js
@@ -3,7 +3,7 @@
  * This file centralizes version management for the application
  */
 
-export const APP_VERSION = '1.0.0'
+export const APP_VERSION = '1.0.1'
 
 /**
  * Get the formatted version string
